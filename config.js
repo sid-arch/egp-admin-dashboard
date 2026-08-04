19e6dd889da642793f1e79f22a9c71d32f6c2319
sid-arch <74917367+sid-arch@users.noreply.github.com>
2026-08-04 12:26:01 -0500

    Update GAS_URL with the new Web App URL

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste your Apps Script Web App URL here
-const GAS_URL='';
+const GAS_URL='https://script.google.com/macros/s/AKfycbxGXDNiTVhhpmdIvRAVO-YLvli-tV1hSBdfipdMzuZDa_SGEv_hHpD7QJsRiU4GgsezFA/exec';
