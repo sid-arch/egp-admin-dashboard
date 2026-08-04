ae40873b7c125deb523a61fdaef50b5da31f9829
sid-arch <74917367+sid-arch@users.noreply.github.com>
2026-08-04 12:37:59 -0500

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@
   };
 */
 const CONFIG = {
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbxGXDNiTVhhpmdIvRAVO-YLvli-tV1hSBdfipdMzuZDa_SGEv_hHpD7QJsRiU4GgsezFA/exec",
   STUDY_START_DATE: "2026-08-06",
   TOTAL_SESSIONS: 12
 };
