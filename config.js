fdf3017779ac50bded0750663c3ef4146187ca28
sid-arch <74917367+sid-arch@users.noreply.github.com>
2026-08-04 12:54:16 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "", // Paste your deployed Apps Script /exec URL here
+  API_URL: "https://script.google.com/macros/s/AKfycbxGXDNiTVhhpmdIvRAVO-YLvli-tV1hSBdfipdMzuZDa_SGEv_hHpD7QJsRiU4GgsezFA/exec", // Paste your deployed Apps Script /exec URL here
   STUDY_START_DATE: "2026-08-06",
   TOTAL_SESSIONS: 12,
   PUBLIC_PAGE: "leaderboard-view.html"
